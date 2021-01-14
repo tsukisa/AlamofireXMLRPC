5c9fac007f4a43d2fe79eb5a043bc8e70056ac46
Casey Law <sacred0214@gmail.com>
2021-01-14 12:52:04 +0800

    update swift-tool-version to 5.3, update alamofire and aexml dependency versions

--- Package.swift
+++ Package.swift
@@ -1,13 +1,26 @@
-// swift-tools-version:3.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AlamofireXMLRPC",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12),
+        .macOS(.v10_11),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 4),
-        .Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4)
-    ]
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.1")),
+        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.6.0")),
+    ],
+    targets: [
+        .target(
+            name: "AlamofireXMLRPC",
+            dependencies: ["Alamofire", "AEXML"],
+            path: "Sources/AlamofireXMLRPC"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
 
